00226883f32590b960e0028f51088ea851aa03e9
Kelvin Wong <kelvinjjwong@outlook.com>
2024-11-21 18:55:55 +0800

    Initial Commit

---
+++ CocoaGifViewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CocoaGifViewer
+//
+//  Created by Kelvin Wong on 2024/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ CocoaGifViewerTests/CocoaGifViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaGifViewerTests.swift
+//  CocoaGifViewerTests
+//
+//  Created by Kelvin Wong on 2024/11/21.
+//
+
+import Testing
+@testable import CocoaGifViewer
+
+struct CocoaGifViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
