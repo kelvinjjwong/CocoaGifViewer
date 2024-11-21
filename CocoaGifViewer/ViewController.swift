241486ad2d123f7747ab1c20b6adcd4400dfb775
Kelvin Wong <kelvinjjwong@outlook.com>
2024-11-21 19:19:10 +0800

    first test

--- CocoaGifViewer/ViewController.swift
+++ CocoaGifViewer/ViewController.swift
@@ -1,20 +1,57 @@
 //
 //  ViewController.swift
-//  CocoaGifViewer
+//  SwiftyGifMacExample
 //
-//  Created by Kelvin Wong on 2024/11/21.
+//  Created by Carlo Rapisarda on 2020-08-04.
+//  Copyright © 2020 alexiscreuzot. All rights reserved.
 //
 
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet private var gifImageView: NSImageView!
+    
+    let gifManager = SwiftyGifManager(memoryLimit:100)
+    let images = [
+        "https://media.giphy.com/media/5tkEiBCurffluctzB7/giphy.gif",
+        "2.gif",
+        "https://media.giphy.com/media/5xtDarmOIekHPQSZEpq/giphy.gif",
+        "3.gif",
+        "https://media.giphy.com/media/3oEjHM2xehrp0lv6bC/giphy.gif",
+        "5.gif",
+        "https://media.giphy.com/media/l1J9qg0MqSZcQTuGk/giphy.gif",
+        "4.gif",
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.viewGif(self.currentI)
+    }
+    
+    var currentI = 0
+    
+    func viewGif(_ i:Int) {
+        let imageSource = images[i]
+        
+        if let image = try? NSImage(imageName: imageSource) {
+            gifImageView.setImage(image, manager: gifManager, loopCount: -1)
+        } else if let url = URL.init(string: imageSource) {
+            gifImageView.setGifFromURL(url, showLoader: true)
+        } else {
+            gifImageView.clear()
+        }
     }
 
+    @IBAction func onNextClicked(_ sender: NSButton) {
+        self.currentI += 1
+        if self.currentI > (images.count-1) {
+            self.currentI = 0
+        }
+        self.viewGif(self.currentI)
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
---
+++ README.md
@@ -0,0 +1,7 @@
+# CocoaGifViewer
+
+This is just a macOS demo of SwiftyGif project.
+
+https://github.com/alexiscreuzot/SwiftyGif
+
+to test with newer version of Xcode and Swift.
